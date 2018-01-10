a6b837f8c10e59c7840c21c1a1f60755b4322cd1
ismael <leamsigc@hotmail.com>
2018-01-10 00:56:34 -0500

    add prefixes and fix minor bugs lol

--- lecture59AjaxAndJson/app.js
+++ lecture59AjaxAndJson/app.js
@@ -1,14 +1,12 @@
-
-
 //load customer
-function loadCustomer(e){
+function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET','customer.json',true);
-    xhr.onload = function(){
-        if(this.status === 200 ){
+    xhr.open('GET', 'customer.json', true);
+    xhr.onload = function () {
+        if (this.status === 200) {
             const response = JSON.parse(this.response);
-            
+
             document.querySelector('#customer').innerHTML = `<ul>
                                                             <li>${response.name}</li>
                                                             <li>${response.company}</li>
@@ -19,27 +17,27 @@ function loadCustomer(e){
     xhr.send();
 }
 
-document.querySelector('#button1').addEventListener('click',loadCustomer,false);
+document.querySelector('#button1').addEventListener('click', loadCustomer, false);
 
 //get customers
-document.querySelector('#button2').addEventListener('click',getAllCostumers,false);
+document.querySelector('#button2').addEventListener('click', getAllCostumers, false);
 
 //get all customers
 function getAllCostumers() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET','customers.json',true);
-    xhr.onload = function(){
-        if(this.status === 200){
+    xhr.open('GET', 'customers.json', true);
+    xhr.onload = function () {
+        if (this.status === 200) {
             let output = `<ul> `;
             const customers = JSON.parse(this.responseText);
-            customers.map( customer => {
+            customers.map(customer => {
                 output += `
                 <li>Name:${customer.name}</li>    
                 <li>Company:${customer.company}</li>    
                 <li>ID:${customer.id}</li>    
                 `;
             });
-            output+= '</ul>';
+            output += '</ul>';
             document.querySelector('#customers').innerHTML = output;
         }
     };
