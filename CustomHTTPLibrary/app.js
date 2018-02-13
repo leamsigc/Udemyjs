fc6f1966183053071e2b7c2acffa1bc4f22c8e50
leamsigc <leamsigc@hotmail.com>
2018-02-13 00:49:00 -0500

    finish the custom http library

--- CustomHTTPLibrary/app.js
+++ CustomHTTPLibrary/app.js
@@ -1,14 +1,57 @@
-const call  = new customHTTP();
+const call = new customHTTP();
 // const post = call.get('https://jsonplaceholder.typicode.com/posts');
 // console.log(post);
 
+// call.get("https://jsonplaceholder.typicode.com/posts", function(
+//   error,
+//   response
+// ) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(response);
+//   }
+// });
 
-call.get('https://jsonplaceholder.typicode.com/posts', function(error,response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log(response);
-    }
+// /*========= GET SINGLE POST ========*/
+
+// call.get("https://jsonplaceholder.typicode.com/posts/1",function(error,post){
+//     if (error) {
+//         console.log(error);
+//     }else{
+//         console.log(post);
+//     }
+// });
+
+// /*========= post request custom library========*/
+const data = {
+    title:'Custom post',
+    body:'This is a custom post'
+};
+// call.post('https://jsonplaceholder.typicode.com/posts',data,function(error,resp){
+//     if(error)
+//     { console.log(error);}
+//     else    {
+//         console.log(resp);
+//     }
+// });
+
+
+ /** 
+  * update a post
+ * */ 
+call.put('https://jsonplaceholder.typicode.com/posts/2',data,function(error,resp){
+    if(error) console.log(error);
+    else console.log(resp); 
 });
 
-call.post();
+/** 
+ * DELETE A POST 
+ * */ 
+call.delete("https://jsonplaceholder.typicode.com/posts/1",function(err,resp){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(resp);
+    }
+});
--- CustomHTTPLibrary/customHTTP.js
+++ CustomHTTPLibrary/customHTTP.js
@@ -1,36 +1,67 @@
-function  customHTTP(){
-    this.http = new XMLHttpRequest();
+function customHTTP() {
+  this.http = new XMLHttpRequest();
 }
 //get request
-customHTTP.prototype.get = function(url,callback){
-    this.http.open('GET',url,true);
+customHTTP.prototype.get = function(url, callback) {
+  this.http.open("GET", url, true);
 
-    let self = this;
+  let self = this;
 
-
-    this.http.onload = function(){
-
-        if (this.status === 200){
-           callback(null,this.responseText);
-        }else{
-            callback('Error:' + self.http.status);
-        }
+  this.http.onload = function() {
+    if (this.status === 200) {
+      callback(null, this.responseText);
+    } else {
+      callback("Error:" + self.http.status);
     }
-    this.http.send();
+  };
+  this.http.send();
 };
 //post request
-customHTTP.prototype.post = function(url,dataToAdd,callBack){
-    this.http.open('POST',url,true);
+customHTTP.prototype.post = function(url, dataToAdd, callBack) {
+  this.http.open("POST", url, true);
 
-    this.http.setRequestHeader('Content-type','application/json');
+  this.http.setRequestHeader("Content-type", "application/json");
 
-    let self = this;
+  let self = this;
 
-    this.http.onload = function(){
-        callBack(null, self.http.responseText);
-    };
+  this.http.onload = function() {
+    callBack(null, self.http.responseText);
+  };
 
-    this.http.send(JSON.stringify(dataToAdd));
+  this.http.send(JSON.stringify(dataToAdd));
 };
-//put request
-//Delete request
+/**
+ * Make a HTTP PUT
+ * REQUEST
+*/
+customHTTP.prototype.put = function(url,data,callBack) {
+  this.http.open('PUT',url,true);
+  this.http.setRequestHeader("Content-type","application/json");
+
+  let self = this;
+
+  this.http.onload = function () {
+    callBack(null,self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+  
+}
+
+/***
+ * Delete a post request 
+ * 
+ * ***/ 
+customHTTP.prototype.delete = function(url,callback){
+  this.http.open("DELETE",url,true);
+
+  let self = this;
+  this.http.onload = function(){
+    if(self.http.status === 200){
+      callback(null,"Post deleted");
+    }else{
+      callback('Error:'+self.http.status);
+    }
+  };
+  this.http.send();
+};
