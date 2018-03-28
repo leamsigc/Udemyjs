66329bfcaf88ca9b7b9558583f438150d3b4f8bd
leamsigc <leamsigc@hotmail.com>
2018-03-27 20:40:14 -0400

    Lecture 65

--- lecture65/promise.js
+++ lecture65/promise.js
@@ -2,48 +2,55 @@
 const demo = document.querySelector('.demo-js');
 
 const posts = [{
-    title: 'Title 1',
-    body: 'body 1'
+        title: 'Title 1',
+        body: 'body 1'
   },
-  {
-    title: 'Title 2',
-    body: 'body 2'
+    {
+        title: 'Title 2',
+        body: 'body 2'
   }
 ];
 //create post
 function createPost(post) {
-  //new promise
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      posts.push(post);
-      const error = false;
-      if (!error) {
-        resolve();
-      } else {
-        reject('Error: something went wrong');
-      }
-    }, 2000)
-  });
+    //new promise
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+            const error = false;
+            if (!error) {
+                resolve();
+            } else {
+                reject('Error: something went wrong');
+            }
+        }, 2000)
+    });
 }
 //get post
 function getPost() {
-  setTimeout(() => {
-    let output = '';
+    setTimeout(() => {
+        let output = '';
+        posts.forEach(post => {
+            output += `<li>${post.title} <br> ${post.body}</li>`;
 
-    posts.forEach(post => {
-      output += `<li>${post.title} <br> ${post.body}</li>`;
-
-    });
-
-    demo.innerHTML = output;
-  }, 1000);
+        });
+        demo.innerHTML = output;
+    }, 1000);
 }
 
 
 //create post
 createPost({
-    title: 'title 3',
-    body: 'body 3'
-  })
-  .then(getPost)
-  .catch(err => console.log(err));
+        title: 'title 3',
+        body: 'body 3'
+    })
+    .then(getPost)
+    .catch(err => console.log(err));
+
+createPost({
+    title:'title 4',
+    body:'body 4'
+})
+.then(getPost)
+.catch(err => {
+    console.log(err);
+});
