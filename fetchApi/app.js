12bc3b3372a08ebc29175c0e90dedf55638fc0b3
leamsigc <leamsigc@hotmail.com>
2018-03-30 15:48:33 -0400

    fetch api sandbox get json from local

--- fetchApi/app.js
+++ fetchApi/app.js
@@ -1,1 +1,31 @@
-console.log('we are connected');
+//button 1
+const btn1 = document.querySelector('#button1');
+const btn2 = document.querySelector('#button2');
+const btn3 = document.querySelector('#button3');
+//output
+const output = document.querySelector('#output');
+btn1.addEventListener('click',getText);
+btn2.addEventListener('click',getJson);
+
+function getText(){
+  //fetch the text file
+  fetch('text.txt')
+  .then(resp => resp.text())
+  .then(data => {
+    console.log(data);
+    output.innerHTML = `Data Recive From The Call: <strong>${data}</strong>`;
+  });
+};
+
+//get Json file
+function getJson(){
+  //fetch the text file
+  fetch('app.json')
+  .then(resp => resp.json())
+  .then(resp => {
+    const li = resp.data.map(item => {
+      return `<li>Name: ${item.name}<br><strong> ${item.position}</strong></li>`
+    });
+    output.innerHTML = `<ul>${li.join('')}</ul>`;
+  });
+};
